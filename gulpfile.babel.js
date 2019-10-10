f37f6f5c784cc7aedab4dd1104be2bf081a4830c
DS Hwang <hds2g22@gmail.com>
2019-10-11 00:50:55 +0900

    Gulp #2: autoprefixer, csso, watchFiles

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,40 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+import del from "del";
 
 const paths = {
   styles: {
     src: "assets/scss/style.scss",
-    dest: "src/static/styles"
+    dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss"
   }
 };
 
-export function styles() {
+/*
+function clean() {
+  del("src/static");
+}
+*/
+function styles() {
   return gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+function watchFiles() {
+  gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
