d5adecdeafcf907f0b6fb757019946f3844a1d40
DS Hwang <hds2g22@gmail.com>
2019-10-20 15:56:17 +0900

    Notification

--- src/socketController.js
+++ src/socketController.js
@@ -1,9 +1,16 @@
 import events from "./events";
 
 const socketController = socket => {
+  //console.log("socketController");
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
+
   socket.on(events.setNickname, ({ nickname }) => {
-    console.log(nickname);
+    console.log(nickname, " connected");
+    broadcast(events.newUser, { nickname });
     socket.nickname = nickname;
+  });
+  socket.on(events.disconnect, () => {
+    broadcast(events.disconnected, { nickname: socket.nickname });
   });
 };
 
